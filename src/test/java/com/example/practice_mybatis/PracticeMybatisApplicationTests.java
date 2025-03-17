1cb636b5003c374ee09a7f1d91d82ad8ca4f67f3
taeyoung kang <kty1467@naver.com>
2025-03-17 17:17:00 +0900

    기본 세팅

---
+++ src/test/java/com/example/practice_mybatis/PracticeMybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practice_mybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeMybatisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
