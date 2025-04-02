b19c61fea23a51ddc63df311940f0bf5c86e8030
taeyoung kang <kty1467@naver.com>
2025-04-02 17:06:02 +0900

    10~11강 수행 완료

--- src/main/java/com/example/practice_mybatis/controller/BoardController.java
+++ src/main/java/com/example/practice_mybatis/controller/BoardController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -48,6 +49,17 @@ public class BoardController {
         model.addAttribute("boardList", boardDTOList);
 //        System.out.print("boardDTOList = " + boardDTOList);
         return "list";
+    }
+
+    @GetMapping("/{id}")
+    public String findById(@PathVariable("id") Long id, Model model) {
+        // 조회수 처리
+        boardService.updateHits(id);
+        // 상세 내용 가져오기
+        BoardDTO boardDTO = boardService.findById(id);
+        model.addAttribute("board", boardDTO);
+
+        return "detail";
     }
 
 
